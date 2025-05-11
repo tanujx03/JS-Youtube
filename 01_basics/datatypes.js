e420253690c2f7336182dc1252790c99fcb6f101
Tanuj <210567004+tanujxgit@users.noreply.github.com>
2025-05-11 17:13:14 +0530

    datatypes in JS

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello")
+// This will not work in Node - there are other way to do this.
+// works fine in browser
+
+// code readibility should be high
+
+let name = "tanuj"; //String
+let marks = 23; //Number
+/*
+number => 2 ** 53
+bigInt
+string = ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => Unique
+*/
+
+/*
+Object
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
