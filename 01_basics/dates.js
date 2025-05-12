0a07934dd1b0220a657e7489981e6aad4ba6e03c
tanuj <210567004+tanujxgit@users.noreply.github.com>
2025-05-12 13:34:29 +0530

    Add dates.js with examples of date creation and formatting

---
+++ 01_basics/dates.js
@@ -0,0 +1,15 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+
+let myCreatedDate = new Date(2023, 24, 23);
+console.log(myCreatedDate);
+console.log(myCreatedDate.toTimeString());
+console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toString());
