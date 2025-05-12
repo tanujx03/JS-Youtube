1ab23dfe0c5bb9959814f83478bc8ed7d6ba35f1
tanuj <210567004+tanujxgit@users.noreply.github.com>
2025-05-12 07:58:19 +0530

    Add initial implementation of Strings.js with string manipulation examples

---
+++ 01_basics/Strings.js
@@ -0,0 +1,17 @@
+const name = "Tanuj";
+const repoCount = 2;
+
+// console.log((name+repoCount+" Value")); => Old Method
+
+// console.log(`Hello my Name is ${name} and my repo count is ${repoCount}`); // new Method
+
+const gameName = new String("TanujDc");
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('T'));
+
