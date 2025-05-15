a2b6be6ed9d4affe2ea3ecab08ccaff189999b25
tanuj <210567004+tanujxgit@users.noreply.github.com>
2025-05-15 10:28:18 +0530

    Add object destructuring example with student object

---
+++ 02_basics/object_destructuring.js
@@ -0,0 +1,17 @@
+const student = {
+    name: "Amit Sharma",
+    rollNumber: 23,
+    className: "10th Grade",
+    section: "B",
+    age: 15,
+    gender: "Male",
+    attendance: 92, // percentage
+    isPrefect: false
+};
+
+// student.subjects
+
+const {name:na} = student;
+// console.log(name);
+console.log(na);
+
