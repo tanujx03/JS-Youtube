8b738db7417beaa1dbb50ad26b8fb2dc3e32f5cb
tanuj <210567004+tanujxgit@users.noreply.github.com>
2025-05-20 10:39:37 +0530

    Format digital clock display to use 12-hour time with locale settings

--- 07_Projects/3-DigitalClock/chaiaurcode.js
+++ 07_Projects/3-DigitalClock/chaiaurcode.js
@@ -2,6 +2,8 @@ const clock = document.querySelector("#clock");
 
 setInterval(() => {
   let date = new Date();
-  clock.innerHTML = date.toLocaleTimeString();
+  clock.innerHTML = date.toLocaleTimeString("en-US", {
+    hour12: true,
+  });
   console.log(date);
 }, 1000);
