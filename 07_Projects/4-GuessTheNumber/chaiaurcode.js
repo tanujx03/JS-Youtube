543401e04b12485d9dabb105f590cfac70837c49
tanuj <210567004+tanujxgit@users.noreply.github.com>
2025-05-20 10:39:28 +0530

    Add Number Guessing Game with HTML, CSS, and JavaScript implementation

---
+++ 07_Projects/4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,51 @@
+const submitButton = document.querySelector("#subt");
+const guessesDisplay = document.querySelector(".guesses");
+const remainingAttemptsDisplay = document.querySelector(".lastResult");
+const hintDisplay = document.querySelector(".lowOrHi");
+
+let guessCount = 0;
+
+// Generate a random number between 1 and 100
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 100 + 1);
+};
+
+// Provide hint to the user
+const provideHint = (userGuess) => {
+  if (randomNumber < userGuess) {
+    hintDisplay.innerHTML = "Number is actually smaller than you think";
+  } else {
+    hintDisplay.innerHTML = "Number is actually greater than you think";
+  }
+};
+
+// Check user's guess and update UI accordingly
+const handleGuess = (userGuess) => {
+  if (randomNumber !== userGuess) {
+    guessCount++;
+    if (guessCount <= 10) {
+      guessesDisplay.innerHTML = guessCount;
+      remainingAttemptsDisplay.innerHTML = 10 - guessCount;
+      provideHint(userGuess);
+    } else {
+      hintDisplay.innerHTML = "Reload the Game";
+    }
+  } else {
+    hintDisplay.innerHTML = "Correct! You guessed the number.";
+  }
+};
+
+// Main function to run on form submission
+const processUserGuess = (event) => {
+  event.preventDefault();
+  const userGuess = parseInt(document.querySelector(".guessField").value);
+
+  if (isNaN(userGuess)) {
+    hintDisplay.innerHTML = "Enter a valid number";
+  } else {
+    handleGuess(userGuess);
+  }
+};
+
+const randomNumber = generateRandomNumber();
+submitButton.addEventListener("click", processUserGuess);
